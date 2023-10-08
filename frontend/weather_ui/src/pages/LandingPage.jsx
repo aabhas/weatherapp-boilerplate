b26413213d810fb46017091a056f5a825dd4494a
Aabhas Sharma <3515900+aabhas@users.noreply.github.com>
2023-10-07 17:10:12 -0700

    Using axios middleware to establish a connection

--- frontend/weather_ui/src/pages/LandingPage.jsx
+++ frontend/weather_ui/src/pages/LandingPage.jsx
@@ -1,27 +1,61 @@
+import React from 'react';
 import sun_icon from './../graphics/sun-regular.svg';
+import { getWeatherData } from './../services/weather.js';
 
-function LandingPage() {
+class LandingPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            temperatureData: '' 
+        };
+
+        this.handleWeatherFetch = this.handleWeatherFetch.bind(this);
+
+    }
+
+    async handleWeatherFetch() {
+
+        const data = await getWeatherData()
+
+        if(data) {
+            console.log('in react component: ' + data)
+            this.setState({
+                temperatureData: data[0].type + "'F"
+            })
+        }
+
+        else {
+            console.log('in react component: I did nothing')
+        }
+        
+    }
     
-    return(
-        <div>
-            
-            <header className="appHeader">
-                <img src={sun_icon} className="Sun-icon" alt="sun_icon" />
-                <p>
-                    Check the weather.
-                </p>
-            </header>
+    render() {
+        return(
+            <div>
+                
+                <header className="appHeader">
+                    <img src={sun_icon} className="Sun-icon" alt="sun_icon" />
+                    <p>
+                        The weather in San Francisco is {this.state.temperatureData}.
+                    </p>
+                </header>
 
-            <div className='weatherWidget'>
+                <div className='weatherWidget'>
 
-                <button className="weatherButton">
-                    Refresh weather data
-                </button>
+                    <button 
+                        className="weatherButton"
+                        onClick={this.handleWeatherFetch}
+                    >
+                        Refresh weather data
+                    </button>
 
+                </div>
+                
             </div>
-            
-        </div>
-    )
+        );
+    }
 
 }
 
